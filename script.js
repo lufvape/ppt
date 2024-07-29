ca4a6d7d0d7c95bd919e8e050d3c34137fd9f20d
Luis Felipe Vargas Perilla <luisfvargas@gmail.com>
2024-07-29 18:12:25 -0500

    toda la interfaz terminada, juto con la funcionalidad de los botones

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let puntajeHumano = 0;
+let puntajeMaquina = 0;
+
+const piedra = document.querySelector("#piedra");
+piedra.addEventListener("click", function(){
+    maquina = getComputerChoice();
+    ronda = playRound(maquina, 'piedra');
+    document.querySelector(".ronda").textContent = ronda;
+    document.querySelector(".footer").textContent = "el resultado del juego es: Máquina: " + puntajeMaquina + " Humano: " + puntajeHumano;
+})
+
+const papel = document.querySelector("#papel");
+papel.addEventListener("click", function(){
+    maquina = getComputerChoice();
+    ronda = playRound(maquina, 'papel');
+    document.querySelector(".ronda").textContent = ronda;
+    document.querySelector(".footer").textContent = "el resultado del juego es: Máquina: " + puntajeMaquina + " Humano: " + puntajeHumano;
+})
+
+const tijera = document.querySelector("#tijera");
+tijera.addEventListener("click", function(){
+    maquina = getComputerChoice();
+    ronda =  playRound(maquina, 'tijera');
+    document.querySelector(".ronda").textContent = ronda;
+    document.querySelector(".footer").textContent = "el resultado del juego es: Máquina: " + puntajeMaquina + " Humano: " + puntajeHumano;
+})
+
 function getComputerChoice(){
     let choice = Math.random();
     if(choice <= 0.33){
@@ -12,66 +39,53 @@ function getComputerChoice(){
     return choice;
 }
 
-function getHumanChoice(){
-    let eleccion = prompt("elije entre piedra, papel o tijera");
-    eleccion = eleccion.toLowerCase();
-    return eleccion;
-}
-
 function playRound(maquina, humano){
-    let resultado = null;
-    switch(humano == 'piedra'){
-        case maquina == 'papel':
-            resultado = 'perdiste';
-            break;
-        case maquina == 'tijera':
-            resultado = 'ganaste!';
-            break;
-        default :
-            resultado = 'empate';
+    let resultado = '';
+    if(puntajeHumano == 5){
+        alert("El juego ha terminado. Ganaste")
     }
+    else if(puntajeMaquina == 5){
+        alert("El juego ha terminado. Perdiste")
+    }
+    else{
+        switch(humano == 'piedra'){
+            case maquina == 'papel':
+                resultado = 'perdiste';
+                break;
+            case maquina == 'tijera':
+                resultado = 'ganaste!';
+                break;
+            default :
+                resultado = 'empate';
+        }
 
-        switch(humano == 'papel'){
-        case maquina == 'piedra':
-            resultado = 'ganaste!';
-            break;
-        case maquina == 'tijera':
-            resultado = 'perdiste!';
-            break;
-        default :
-            resultado = 'empate';
-    }
+            switch(humano == 'papel'){
+            case maquina == 'piedra':
+                resultado = 'ganaste!';
+                break;
+            case maquina == 'tijera':
+                resultado = 'perdiste!';
+                break;
+            default :
+                resultado = 'empate';
+        }
 
-        switch(humano == 'tijera'){
-        case maquina == 'piedra':
-            resultado = 'perdiste';
-            break;
-        case maquina == 'papel':
-            resultado = 'ganaste!';
-            break;
-        default :
-        resultado = 'empate';
+            switch(humano == 'tijera'){
+            case maquina == 'piedra':
+                resultado = 'perdiste';
+                break;
+            case maquina == 'papel':
+                resultado = 'ganaste!';
+                break;
+            default :
+            resultado = 'empate';
+        }
+        if(resultado == 'ganaste!'){
+            puntajeHumano ++;
+        }
+        else if(resultado == 'perdiste'){
+            puntajeMaquina ++;
+        }
     }
-    console.log(resultado);
     return resultado;  
 }
-
-function playGame(){
-    let puntajeHumano = 0;
-    let puntajeMaquina = 0;
-    let i = 0;
-    while(i<5){
-        let ronda = playRound(getComputerChoice(), getHumanChoice());
-        if(ronda == 'ganaste!'){
-            puntajeHumano ++;
-            i++;
-        }
-        else if(ronda == 'perdiste'){
-            puntajeMaquina ++;
-            i++;
-        }
-        else i++;
-    }
-    return 'el resultado del juego es: Máquina:' + puntajeMaquina + 'Humano:' + puntajeHumano;
-}
-console.log(playGame());
